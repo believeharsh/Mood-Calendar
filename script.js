a605be60af580db30355e912d16d41af11b78c68
Bontinue <93024harsh@gmail.com>
2023-07-11 10:36:18 +0530

    added some style on floting container

--- script.js
+++ script.js
@@ -148,15 +148,3 @@ function addDays(date, days) {
 	return result;
 }
 
-// SOCIAL PANEL JS
-const floating_btn = document.querySelector('.floating-btn');
-const close_btn = document.querySelector('.close-btn');
-const social_panel_container = document.querySelector('.social-panel-container');
-
-floating_btn.addEventListener('click', () => {
-	social_panel_container.classList.toggle('visible')
-});
-
-close_btn.addEventListener('click', () => {
-	social_panel_container.classList.remove('visible')
-});
